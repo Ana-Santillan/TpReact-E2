7a1ffd62e35a0ee55df05c5d9ab31e4d33ac3c6f
Ana Santillan <anisantillan10@gmail.com>
2023-06-01 20:35:45 -0300

    estructura inicial a modificar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Holamundo from './components/Holamundo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <section className='container'>
+      <Holamundo></Holamundo>
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Holamundo.jsx
@@ -0,0 +1,9 @@
+const Holamundo = () => {
+    return (
+        <section className="text-center my-5">
+            <h1 className='display-4'>Hola Mundo!</h1>
+        </section>
+    );
+};
+
+export default Holamundo;
